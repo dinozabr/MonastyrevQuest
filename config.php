a6454a67ecdfecf99ef5ad981c16b2641fd659a7
dinozabr <yacik1996@gmail.com>
2019-01-22 03:05:22 +1000

    new heroku config

--- config.php
+++ config.php
@@ -10,14 +10,10 @@
 
 
 if ($_SERVER['SERVER_NAME'] == "thawing-stream-94645.herokuapp.com") {
-
-
-
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $dbname = substr($url["path"], 1);
+    $host = 'us-cdbr-iron-east-01.cleardb.net';
+    $username = 'b96f3dfb1de729';
+    $password = '015e215c';
+    $dbname = 'heroku_49c5e89e0448e08';
 } else {
 
     $host = 'localhost';
