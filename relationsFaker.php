12a50f39905f3cd97938f8f7d9f69e3d90159ad3
dinozabr <yacik1996@gmail.com>
2019-01-22 13:36:11 +1000

    result commit

--- relationsFaker.php
+++ relationsFaker.php
@@ -8,94 +8,67 @@
 require __DIR__ . '/vendor/fzaninotto/faker/src/autoload.php';
 require __DIR__ . '/config.php';
 
-//$link = mysqli_connect("localhost", "root", "madahyryt", "testQuest");
-//$link->set_charset("utf8");
 $link = mysqli_connect($host, $username, $password, $dbname);
 
 $link->set_charset("utf8");
-$act =$_POST['action'];
+
 $act = json_decode($_POST['data']);
 $items = Array();
 $items1 = Array();
 
-function CreateTree($array,$sub)
-{
-    $a = array();
-
-    foreach($array as $v)
+    function CreateTree($array,$sub)
     {
-        if($sub == $v['head_id'])
+        $a = array();
+
+        foreach($array as $v)
         {
-            $b = CreateTree($array,$v['id']);
-            if(!empty($b))
-                $a[$v['name']] = $b;
-            else
-                $a[$v['name']] = $b;
-        }
-
-    }
-
-
-    return $a;
-}
-
-
-if ($act->action == 'getListName') {
-    $query = "SELECT name FROM ". $dbname.".employers";
-
-    $result = $link->query($query);
-    while ($line = mysqli_fetch_assoc($result)) {
-        $items[] = $line;
-    }
-    echo $js = json_encode($items, JSON_UNESCAPED_UNICODE);
-}
-else  if($act->action == 'getListFromEmployer')
-    {
-        $name = $act->name;
-
-        $query = "SELECT * FROM ". $dbname.".employers";
-        $name = $act->name;
-        $idEmployer = -1;
-        $head_id = -1;
-
-        $listAll = $link->query($query);
-
-
-        while ($line = mysqli_fetch_assoc($listAll)) {
-            $items[] = $line;
-            if ($line['name'] == $name) {
-                $idEmployer = $line['id'];
-                $head_id = $line['head_id'];
+            if($sub == $v['head_id'])
+            {
+                $b = CreateTree($array,$v['id']);
+                if(!empty($b))
+                    $a[$v['name']] = $b;
+                else
+                    $a[$v['name']] = $b;
             }
 
         }
-
-
-        $tree=CreateTree($items,$head_id);
-        //print_r($firstElement);
-        //array_unshift($tree,$firstElement,);
-        //print_r($firstElement);
-        $test_array[$name] = $tree[$name];
-//        $test_array= $tree[$name];
-
-        //print_r($test_array);
-        echo json_encode($test_array, JSON_UNESCAPED_UNICODE);
-
-
-
+        return $a;
     }
-    else {
-        $query = "SELECT * FROM  ". $dbname.".employers";
 
+
+    if ($act->action == 'getListName') {
+        $query = "SELECT name FROM ". $dbname.".employers ORDER BY name";
         $result = $link->query($query);
-
-
         while ($line = mysqli_fetch_assoc($result)) {
             $items[] = $line;
         }
-        $tree=CreateTree($items,0);
-        echo json_encode($tree, JSON_UNESCAPED_UNICODE);
+        echo $js = json_encode($items, JSON_UNESCAPED_UNICODE);
     }
+    else
+        if($act->action == 'getListFromEmployer') {
+            $query = "SELECT * FROM ". $dbname.".employers ORDER BY name";
+            $name = $act->name;
+            $head_id = -1;
+            $listAll = $link->query($query);
+            while ($line = mysqli_fetch_assoc($listAll)) {
+                $items[] = $line;
+                if ($line['name'] == $name) {
+                    $head_id = $line['head_id'];
+                }
+            }
+            $tree=CreateTree($items,$head_id);
+
+            $test_array[$name] = $tree[$name];
+            echo json_encode($test_array, JSON_UNESCAPED_UNICODE);
+        } else {
+                $query = "SELECT * FROM  ". $dbname.". employers ORDER BY name";
+                $listAll = $link->query($query);
+                while ($line = mysqli_fetch_assoc($listAll)) {
+                    $items[] = $line;
+                }
+                $tree=CreateTree($items,0);
+                echo json_encode($tree, JSON_UNESCAPED_UNICODE);
+        }
 
 
 
---
+++ testQuest_employers.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: testQuest
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employers`
+--
+
+DROP TABLE IF EXISTS `employers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `head_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employers`
+--
+
+LOCK TABLES `employers` WRITE;
+/*!40000 ALTER TABLE `employers` DISABLE KEYS */;
+INSERT INTO `employers` VALUES (193,'Алексей Р.Д',0),(194,'Гордеев Э.В',193),(195,'Марта Д.Д',193),(196,'Логинова Р.С',193),(197,'Афанасьева А.Р',194),(198,'Павлов С.Ф',194),(199,'Жданова З.А',195),(200,'Кудряшова Д.И',195),(201,'Степан Ф.З',196),(202,'Нонна В.М',196),(203,'Доминика И.К',201),(204,'Олег Л.С',201),(205,'Вера Л.Т',197),(206,'Антон Л.В',197),(207,'Калинина Р.Л',198),(208,'Шубина М.А',207),(209,'Нестор М.С',207),(210,'Сава М.А',207),(211,'Кузнецова Е.А',207),(212,'Анисимова И.Ф',199),(213,'Мария В.Л',199),(214,'Артём А.С',200),(215,'Адам Л.Н',200),(216,'Олеся Б.К',205),(217,'Пестова В.Л',205),(218,'Александра Л.З',205),(219,'Милан А.Н',206),(220,'Дмитриева А.Е',203),(221,'Бурова А.Р',204),(222,'Анфиса Б.Л',208);
+/*!40000 ALTER TABLE `employers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-22 13:21:04
