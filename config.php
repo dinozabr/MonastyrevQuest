01bb97f9bf5f980e9c9f8a7a88ccd12221005cee
dinozabr <yacik1996@gmail.com>
2019-01-22 17:05:26 +1000

    make better

--- config.php
+++ config.php
@@ -10,10 +10,16 @@
 
 
 if ($_SERVER['SERVER_NAME'] == "mighty-shelf-66755.herokuapp.com") {
-    $host = 'us-cdbr-iron-east-03.cleardb.net';
-    $username = 'bdcf4b6759d365';
-    $password = '3ecbf07';
-    $dbname = 'heroku_7af04bb30d5d26c';
+//    $host = 'us-cdbr-iron-east-03.cleardb.net';
+//    $username = 'bdcf4b6759d365';
+//    $password = '3ecbf07';
+//    $dbname = 'heroku_7af04bb30d5d26c';
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+
 } else {
 
     $host = 'localhost';
